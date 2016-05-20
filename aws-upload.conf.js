af5e00330e5c9919345eb436da32540c584df005
Luca Hagel <hagel.luca@gmail.com>
2016-05-20 03:56:11 +0200

    Initial Commit

---
+++ aws-upload.conf.js
@@ -0,0 +1,11 @@
+module.exports = {
+	credentials:"aws-credentials.json",
+	bucketName:"eri-creations.com",
+	patterns:[
+		"vendor/**/*",
+		"css/style.css",
+		"img/**/*.jpg",
+    "img/**/*.png",
+		"index.html"
+	]
+}
